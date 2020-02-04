e08d8a33f6d5c9de7578e5b8b9208e3e60057576
RojanPaul <RojanPaul@LAPTOP-8MVRJFDB>
2020-02-04 12:38:50 +0100

    adding backend services

--- src/main/java/com/backend/todo/todoapp/controller/TodoController.java
+++ src/main/java/com/backend/todo/todoapp/controller/TodoController.java
@@ -1,10 +1,13 @@
 package com.backend.todo.todoapp.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.backend.todo.todoapp.model.HardCodedTodoData;
 import com.backend.todo.todoapp.model.Todo;
@@ -37,13 +40,18 @@ public class TodoController {
 
 	}
 
-	@PutMapping(path="/users/{username}/todo")
-	public Todo updateTodo(@PathVariable String username,@RequestBody Todo todo){
-		return hardCodedTodoData.updateById(todo);
+	@PutMapping(path="/users/{username}/todos")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username,@RequestBody Todo todo){
+		Todo todoUpdated = hardCodedTodoData.updateById(todo);
+		return new ResponseEntity<>(todoUpdated,HttpStatus.CREATED);
 	}
 
-	@PostMapping(path="/users/{username}/todo")
-	public Todo saveTodo(@PathVariable String username,@RequestBody Todo todo){
-		return hardCodedTodoData.saveTodo(todo);
+	@PostMapping(path="/users/{username}/todos")
+	public ResponseEntity<Void> saveTodo(@PathVariable String username,@RequestBody Todo todo){
+		Todo createdTodo = hardCodedTodoData.saveTodo(todo);
+		//Location
+		//Get current resource url
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
